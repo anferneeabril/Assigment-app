46b7685ffe560267e51b4d722e16be69d3bb29c6
anferneeabril <anfernee013@gmail.com>
2020-04-15 00:08:17 -0400

    La mala

---
+++ assigned_work.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `assignments` (
+  `id_jobs` int(11) NOT NULL AUTO_INCREMENT,
+  `random_jobs` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_jobs`)
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4
