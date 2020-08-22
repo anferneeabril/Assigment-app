c0241fc7b7877f6cff4d0296eb53e0fbebb78760
ANFERNEE ABRIL RUPERTO <Aabril4810@intersg.edu>
2020-08-22 18:22:33 -0400

    New things

--- application/controllers/Private_controller.php
+++ application/controllers/Private_controller.php
@@ -18,17 +18,22 @@ class Private_controller extends CI_Controller
 
     public function index()
     {
+        
         $username = $this->session->userdata('username');
-
-        echo '<br /><br /><br /><h1 align="center">Welcome ';
-        echo $username;
-        echo '</h1>';
-        echo '<p align="center"><a href="'.base_url().'Private_controller/logout">Logout</a></p>';
+        
+        // echo '<br /><br /><br /><h1 align="center">Welcome ';
+        // echo $username;
+        // echo '</h1>';
+        // echo '<p align="center"><a href="'.base_url().'Private_controller/logout">Logout</a></p>';
+        
         
         $tableUser = new User_model();
-        $sendUserTabletoView['username'] = $this->User_model->getTable($username);
 
-        $this->load->view('Auntentication_view', $sendUserTabletoView);
+        $usernameConnected['usernameLogin'] = $this->User_model->getTable('user');
+        $this->load->view('HeaderHome_view', $usernameConnected);
+        
+        $sendUserTabletoView['username'] = $this->User_model->getTable($username);
+        $this->load->view('ContentHome_view', $sendUserTabletoView);
     }
 
     public function logout()
--- application/views/ContentHome_view.php
+++ application/views/ContentHome_view.php
@@ -2,7 +2,7 @@
 
 foreach ($username as $usernameAccount)
 {
-	echo "<ul class='list-group'>";
+	echo "<br><ul class='list-group'>";
 	echo "<li class= list-group-item active'>$usernameAccount->Lunes";
 	echo "<li class= list-group-item active'>$usernameAccount->Martes";
 	echo "</ul>";
